f47854c84c40ccfe4e7fc28d767937c14cef6d58
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-01-15 11:48:28 +0100

    Improve the documentation of Flux_zip

--- lib/flux_zip.mli
+++ lib/flux_zip.mli
@@ -13,5 +13,9 @@ val of_filepath :
   -> string
   -> string Flux.source
   -> entry
+(** [of_filepath filepath src] makes a new {!type:entry} from a file whose
+    content is consumable via [src] (you can use {!val:Flux.Source.file}). *)
 
 val zip : (entry, string) Flux.flow
+(** [zip] It converts entries into a byte stream corresponding to the ZIP64
+    format. *)
