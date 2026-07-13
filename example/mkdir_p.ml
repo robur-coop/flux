66a927947425cb83f2fbe8a694b63aed1a134024
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-07-13 11:16:28 +0200

    Add a simple test about our zip/unzip

---
+++ example/mkdir_p.ml
@@ -0,0 +1,21 @@
+let mkdir dirpath perm =
+  try Sys.mkdir dirpath perm with 
+  | Sys_error _ when Sys.file_exists dirpath && Sys.is_directory dirpath -> ()
+  | exn -> raise exn
+
+let mkdir_p path perm =
+  let open Filename in
+  let rec split_path acc p =
+    let parent = dirname p in
+    let base = basename p in
+    if p = parent || p = "." || p = "/" then
+      if p = "/" then "/" :: acc else acc
+    else split_path (base :: acc) parent
+  in
+  let components = split_path [] path in
+  let fn current_base dir =
+    let next_path = concat current_base dir in
+    mkdir next_path perm;
+    next_path in
+  let _ = List.fold_left fn (if path.[0] = '/' then "/" else ".") components in
+  ()
--- example/unzip.ml
+++ example/unzip.ml
@@ -1,3 +1,5 @@
+open Mkdir_p
+
 let archive = ref None
 let output = ref String.empty
 
@@ -10,28 +12,6 @@ let usage = Fmt.str "%s [-o directory/] archive.zip" Sys.executable_name
 
 let args =
   [ "-o", Arg.Set_string output, "Extract files into the given directory" ]
-
-let mkdir dirpath perm =
-  try Sys.mkdir dirpath perm with 
-  | Sys_error _ when Sys.file_exists dirpath && Sys.is_directory dirpath -> ()
-  | exn -> raise exn
-
-let mkdir_p path perm =
-  let open Filename in
-  let rec split_path acc p =
-    let parent = dirname p in
-    let base = basename p in
-    if p = parent || p = "." || p = "/" then
-      if p = "/" then "/" :: acc else acc
-    else split_path (base :: acc) parent
-  in
-  let components = split_path [] path in
-  let fn current_base dir =
-    let next_path = concat current_base dir in
-    mkdir next_path perm;
-    next_path in
-  let _ = List.fold_left fn (if path.[0] = '/' then "/" else ".") components in
-  ()
 
 let () =
   Arg.parse args anon usage;
